71d6665173fa4ac640f661f69571eca54b223f2d
Sergey Korsakov <korsakov.serg@gmail.com>
2012-09-23 21:44:36 +0700

    Added new helpers

--- chrome-plugin/functions.js
+++ chrome-plugin/functions.js
@@ -16,6 +16,19 @@ function setUserLocation(location_id, city, region, country, label) {
     $(window).trigger('locationChanged');
 }
 
+function getLocationAsAString(city, region, country, options) {
+    options = options || {};
+    var shortCity = city;
+    if (options && options['cityLength']) {
+        if ( options['cityLength'] < city.length ) {
+            shortCity = city.substring(0, options['cityLength']);
+        }
+    }
+    return shortCity +
+        + ( country == 'US' ? ', ' + region : '' )
+        + ', ' + country;
+}
+
 function requestUserCoordinates ( callback ) {
     if ( navigator && navigator.geolocation ) {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -58,4 +71,64 @@ function getUrlHost( url ) {
 
 function getFaviconUrlByPageUrl( url ) {
     return getUrlHost(url) + '/favicon.ico';
+}
+
+function getTranslation(messageId) {
+    var str = chrome.i18n.getMessage.apply(null, arguments);
+    if (str.length == 0 && TN_CONFIG['env'] == 'debug') {
+        console.log("Could not find translation for message id: " + messageId);
+    }
+    return str;
+}
+
+function getCategoryTranslation(category) {
+    var c = getTranslation('Category_' + category);
+    return c.length > 0 ? c : category;
+}
+
+function storageSet(key, value, target) {
+    var t = target || 'local';
+    var obj = {};
+    obj[key] = value;
+    if ( target == 'local' ) {
+        chrome.storage.local.set(obj);
+    } else {
+        chrome.storage.sync.set(obj);
+    }
+}
+
+function storageRemove(keys, target) {
+    var t = target || 'local';
+    if ( target == 'local' ) {
+        chrome.storage.local.remove(keys);
+    } else {
+        chrome.storage.sync.remove(keys);
+    }
+}
+
+function storageGet(keys, callback) {
+    var t = target || 'local';
+    if ( target == 'local' ) {
+        chrome.storage.local.get(keys, function(items){
+            callback.call(null, items);
+        });
+    } else {
+        chrome.storage.sync.get(keys, function(items){
+            callback.call(null, items);
+        });
+    }
+}
+
+function timestamp_ms() {
+    return (new Date()).getTime();
+}
+
+function timestamp() {
+    return Math.round((new Date()).getTime() / 1000);
+}
+
+function debug(msg) {
+    if ( TN_CONFIG['env'] == 'debug' && console && console.log) {
+        console.log(msg);
+    }
 }
